3b298d7be3311e968f8b40d8396bcded09b68a5f
Sean <89929031+seanpanpan321@users.noreply.github.com>
2024-10-07 20:57:06 -0400

    Revert "google map integration" (#18)

--- app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.home_view, name='home'),
-    path('register/', views.register_view, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-]
--- maps/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('maps/', views.map_view, name='map_view'),
-]
