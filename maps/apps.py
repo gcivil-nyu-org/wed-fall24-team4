253d33c89cf7f25a5e74d4f4c01b00651619cbc2
rsc1102 <rohitchaudhari1102@gmail.com>
2024-10-16 17:12:16 -0400

    configured for travis CI

--- maps/apps.py
+++ maps/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class MapsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'maps'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "maps"
--- stepfreemta/asgi.py
+++ stepfreemta/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stepfreemta.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stepfreemta.settings")
 
 application = get_asgi_application()
