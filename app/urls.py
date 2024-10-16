88cd3e5f212be00a8198e64c8053d04ee839489d
Avina <mauryavina20@gmail.com>
2024-10-16 18:23:28 -0400

    stations

--- app/urls.py
+++ app/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('stations/', views.StationsView.as_view(), name='staions'),
+    path('stations/', views.StationsView.as_view(), name='stations'),
     path('stations/<int:pk>/', views.StationDetailView.as_view(), name='station_detail'),
 ]
